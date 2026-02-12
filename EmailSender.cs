9af731fcdad044e459898ec2c9626d4bf44d5576
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-02-12 21:28:56 +0800

    11.  Move the User Registration form from Register/New to Identity/Register

---
+++ EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        // Implement the SendEmailAsync method from IEmailSender interface
+        /*public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic to send email
+            return Task.CompletedTask;
+        }*/
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient("smtp.hostinger.com", 587)
+            {
+                Credentials = new System.Net.NetworkCredential("noreply@rule1020.dole4a.com", "itDOLE4@rule1020"),
+                EnableSsl = true
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("noreply@rule1020.dole4a.com"),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true
+            };
+            mailMessage.To.Add(email);
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
