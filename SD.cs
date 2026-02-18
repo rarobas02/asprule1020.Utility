4255aaee472f6306bfdf7a32814378bc5835fd9d
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-02-18 22:45:00 +0800

    24. User Roles and Admin page Layout

---
+++ SD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asprule1020.Utility
+{
+    public static class SD
+    {
+        public const string Role_Admin = "IT Admin";
+        public const string Role_Client = "Client";
+        public const string Role_Evaluator = "Evaluator";
+        public const string Role_Po_Head = "PO Head";
+        public const string Role_Region_Focal = "Region Focal";
+
+        public const string StatusForReview = "For Review";
+        public const string StatusForApproval = "For Approval";
+        public const string StatusForReapplication = "For Reapplication";
+        public const string StatusApproved = "Approved";
+        public const string StatusReapplication = "Reapplication";
+
+        public const string SessionCart = "SessionCart";
+    }
+}
