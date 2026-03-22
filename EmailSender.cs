dba7a7b89d5ba20c9de0c6573d858319af15cf9a
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-22 18:01:38 +0800

    63. Approved Email Send

--- EmailSender.cs
+++ EmailSender.cs
@@ -1,37 +1,66 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BulkyBook.Utility
+namespace asprule1020.Utility
 {
     public class EmailSender : IEmailSender
     {
+        //public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        //{
+        //    //logic to send email
+        //    return Task.CompletedTask;
+        //}
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            //logic to send email
-            return Task.CompletedTask;
-        }
-
-        /*public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            var client = new SmtpClient("HostClient URL", 587) // 587 smtp port number - default
+            var client = new SmtpClient("smtp.hostinger.com", 587) // 587 smtp port number - default
             {
-                Credentials = new System.Net.NetworkCredential("SMTP EMAIL HERE", "SMTP PASSWORD HERE"),
+                Credentials = new System.Net.NetworkCredential("noreply@rule1020.dole4a.com", "itDOLE4@rule1020"),
                 EnableSsl = true
             };
+
             var mailMessage = new MailMessage
             {
-                From = new MailAddress("SMTP EMAIL HERE"),
+                From = new MailAddress("noreply@rule1020.dole4a.com"),
                 Subject = subject,
                 Body = htmlMessage,
                 IsBodyHtml = true
             };
+
             mailMessage.To.Add(email);
             return client.SendMailAsync(mailMessage);
-        }*/
+        }
+
+        public async Task SendEmailAsync(
+            string email,
+            string subject,
+            string htmlMessage,
+            byte[] attachmentBytes,
+            string attachmentFileName,
+            string contentType = "application/octet-stream")
+        {
+            using var client = new SmtpClient("smtp.hostinger.com", 587)
+            {
+                Credentials = new System.Net.NetworkCredential("noreply@rule1020.dole4a.com", "itDOLE4@rule1020"),
+                EnableSsl = true
+            };
+
+            using var mailMessage = new MailMessage
+            {
+                From = new MailAddress("noreply@rule1020.dole4a.com"),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(email);
+
+            using var stream = new MemoryStream(attachmentBytes);
+            var attachment = new Attachment(stream, attachmentFileName, contentType);
+            mailMessage.Attachments.Add(attachment);
+
+            await client.SendMailAsync(mailMessage);
+        }
     }
 }
---
+++ ProvinceDetails.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace asprule1020.Utility
+{
+    public static class ProvinceDetails
+    {
+        public static ProvinceContactInfo GetProvinceInfo(string? province)
+        {
+            if (string.IsNullOrWhiteSpace(province))
+            {
+                return new ProvinceContactInfo(
+                    "N/A",
+                    "Provincial Office",
+                    "N/A",
+                    "N/A",
+                    "N/A",
+                    "N/A",
+                    "N/A");
+            }
+
+            return province.Trim() switch
+            {
+                "Cavite" => new ProvinceContactInfo(
+                    "Trece Martires City, Cavite",
+                    "Cavite Provincial Office",
+                    "One FS Industrial Bldg., Hugo Perez, Trece Martires City, Cavite",
+                    "(046) 423-3223 / (046) 419-0830 / (046) 419-1996",
+                    "dole_cavite@yahoo.com / cpo.dole4a@gmail.com",
+                    "MARIVIC B. MARTINEZ",
+                    "Cavite Provincial Director"),
+
+                "Laguna" => new ProvinceContactInfo(
+                    "Calamba City, Laguna",
+                    "Laguna Provincial Office",
+                    "2F Puregold Jr. Bldg. Parian City of Calamba, Laguna",
+                    "(049) 545-7361 / (049) 545-7358",
+                    "lpo_dole4a@yahoo.com / lpo.dole4a@gmail.com",
+                    "GUIDO R. RECIO",
+                    "Laguna Provincial Director"),
+
+                "Batangas" => new ProvinceContactInfo(
+                    "Lipa City, Batangas",
+                    "Batangas Provincial Office",
+                    "Unit L The Olan Business Center, Brgy. Marawoy, Lipa City, Batangas",
+                    "(043) 726-9972",
+                    "ro4a_batangas@dole.gov.ph",
+                    "PREDELMA M. TAN",
+                    "Batangas Provincial Director"),
+
+                "Rizal" => new ProvinceContactInfo(
+                    "Cainta, Rizal",
+                    "Rizal Provincial Office",
+                    "3rd Floor, Ortigas Avenue Extension corner Sampaguita Street, Bayanihan Village, San Isidro, Cainta, Rizal",
+                    "(02) 212-1648",
+                    "dole4a_rpo@yahoo.com / rpo.dole4a@gmail.com",
+                    "CELIA G. ARIOLA",
+                    "Head, Rizal Provincial Office"),
+
+                "Quezon" => new ProvinceContactInfo(
+                    "Lucena City, Quezon",
+                    "Quezon Provincial Office",
+                    "2F Grand Central Terminal, Lucena City, Quezon",
+                    "(042) 785-0894",
+                    "dole4a.quezon@gmail.com",
+                    "EDWIN T. HERNANDEZ",
+                    "OIC Head, Quezon Provincial Office"),
+
+                _ => new ProvinceContactInfo(
+                    "N/A",
+                    $"{province} Provincial Office",
+                    "N/A",
+                    "N/A",
+                    "N/A",
+                    "N/A",
+                    "N/A")
+            };
+        }
+    }
+
+    public sealed record ProvinceContactInfo(
+        string OfficeAddress,
+        string ProvincialOffice,
+        string ProvinceAddress,
+        string ProvinceTelNo,
+        string ProvinceEmail,
+        string PdName,
+        string ProDir);
+}
